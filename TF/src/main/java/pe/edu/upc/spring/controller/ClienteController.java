29bba468427d87594ea8dd704b50671d4df38b33
jhervacio <43193176+jhervacio@users.noreply.github.com>
2021-05-28 20:29:03 -0500

    Prueba

---
+++ TF/src/main/java/pe/edu/upc/spring/controller/ClienteController.java
@@ -0,0 +1,98 @@
+package pe.edu.upc.spring.controller;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.sun.el.parser.ParseException;
+
+import pe.edu.upc.spring.model.Cliente;
+import pe.edu.upc.spring.service.IClienteService;
+
+@Controller
+@RequestMapping("/cliente")
+public class ClienteController {
+	
+	@Autowired
+	private IClienteService rService;
+	
+	@RequestMapping("/bienvenido")
+	public String irPaginaBienvenida() {
+		return "bienvenido";
+	}
+	
+	@RequestMapping("/")
+	public String irPaginaListadoClientes(Map<String, Object> model) {
+		model.put("listaClientes", rService.listar());
+		return "listCliente";
+	}
+	
+	@RequestMapping("/irRegistrar")
+	public String irPaginaRegistrar(Model model) {
+		model.addAttribute("cliente", new Cliente());
+		return "cliente";
+	}
+	
+	@RequestMapping("/registrar")
+	public String registrar(@ModelAttribute Cliente objCliente, BindingResult binRes, Model model) throws ParseException {
+		if (binRes.hasErrors())
+			return "cliente";
+		else {
+			boolean flag = rService.insertar(objCliente);
+			if (flag)
+				return "redirect:/cliente/listar";
+			else {
+				model.addAttribute("mensaje", "Ocurrio un error");
+				return "redirect:/cliente/irRegistrar";
+			}
+		}
+	}
+	
+	@RequestMapping("/modificar/{id}")
+	public String modificar(@PathVariable int id, Model model, RedirectAttributes objRedir) throws ParseException {
+		Optional<Cliente> objCliente = rService.listarId(id);
+		if(objCliente == null) {
+			objRedir.addFlashAttribute("mensaje", "Ocurrio un error");
+			return "redirect:/cliente/listar";
+		}
+		else {
+			model.addAttribute("cliente", objCliente);
+			return "cliente";
+		}
+	}
+	
+	@RequestMapping("/eliminar")
+	public String eliminar(Map<String, Object> model, @RequestParam(value="id") Integer id) {
+		try {
+			if (id != null && id>0) {
+				rService.eliminar(id);
+				model.put("listaClientes", rService.listar());
+			}
+		}
+		catch (Exception ex) {
+			System.out.println(ex.getMessage());
+			model.put("mensaje", "Ocurrio un error");
+			model.put("listaClientes", rService.listar());
+		}
+		return "listCliente";
+	}
+	
+	@RequestMapping("/listar")
+	public String listar(Map<String, Object> model) {
+		model.put("listaClientes", rService.listar());
+		return "listCliente";
+	}
+	
+	
+	
+	
+}
